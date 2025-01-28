5d0ddf205b25d2ddf9874477c2d14a5e16d0ba88
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-01-28 19:37:17 +0800

    simplified

---
+++ SMSL.UI/Controls/TitleBar.axaml.cs
@@ -0,0 +1,106 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+
+namespace SMSL.UI.Controls;
+
+public partial class TitleBar : UserControl
+{
+    public TitleBar()
+    {
+        InitializeComponent();
+        
+        #if DEBUG
+        TitleGrid.ShowGridLines = true;
+        #endif
+    }
+    
+    private void MinimizeButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        root.WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        if (root.WindowState == WindowState.Maximized)
+        {
+            root.WindowState = WindowState.Normal;
+            MaximizeIcon.Text = "\uE922";
+        }
+        else
+        {
+            root.WindowState = WindowState.Maximized;
+            MaximizeIcon.Text = "\uE923";
+        }
+    }
+
+    private void CloseButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        root.Close();
+    }
+    
+    // FIXME
+    private void CloseButton_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        CloseButton.Background = new SolidColorBrush(Color.FromRgb(232, 17, 35));
+        CloseButton.BorderBrush = new SolidColorBrush(Color.FromRgb(232, 17, 35));
+    }
+    
+    // FIXME
+    private void CloseButton_OnPointerExited(object? sender, PointerEventArgs e)
+    {
+        CloseButton.Background = Brushes.Transparent;
+        CloseButton.BorderBrush = Brushes.Transparent;
+    }
+    
+    // Window Drag
+    
+    private bool _isPointerPressed;
+    private PixelPoint _startPosition = new(0, 0);
+    private Point _mouseOffsetToOrigin = new(0, 0);
+
+    private void TitleGrid_StartListenForDrag(object? sender, PointerPressedEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        _startPosition = root.Position;
+        _mouseOffsetToOrigin = e.GetPosition(this);
+        _isPointerPressed = true;
+    }
+
+    private void TitleGrid_HandlePotentialDrag(object? sender, PointerEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        if (!_isPointerPressed)
+        {
+            return;
+        }
+        
+        var pos = e.GetPosition(this);
+        _startPosition = new PixelPoint(
+            (int) (_startPosition.X + pos.X - _mouseOffsetToOrigin.X), 
+            (int) (_startPosition.Y + pos.Y - _mouseOffsetToOrigin.Y));
+        root.Position = _startPosition;
+    }
+
+    private void TitleGrid_HandlePotentialDrop(object? sender, PointerReleasedEventArgs e)
+    {
+        var root = (Window) VisualRoot!;
+        
+        var pos = e.GetPosition(this);
+        _startPosition = new PixelPoint(
+            (int) (_startPosition.X + pos.X - _mouseOffsetToOrigin.X), 
+            (int) (_startPosition.Y + pos.Y - _mouseOffsetToOrigin.Y));
+        root.Position = _startPosition;
+        _isPointerPressed = false;
+    }
+}
--- SMSL.UI/Views/MainWindow.axaml.cs
+++ SMSL.UI/Views/MainWindow.axaml.cs
@@ -1,8 +1,4 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
-using Avalonia.Media;
 using SMSL.UI.ViewModels;
 
 namespace SMSL.UI.Views;
@@ -15,7 +11,6 @@ public partial class MainWindow : Window
         
         #if DEBUG
         WindowGrid.ShowGridLines = true;
-        TitleGrid.ShowGridLines = true;
         MainGrid.ShowGridLines = true;
         #endif
     }
@@ -31,80 +26,5 @@ public partial class MainWindow : Window
         
         ContentPanel.Children.Clear();
         ContentPanel.Children.Add(((MainWindowViewModel) DataContext!).SideBarPages[SideBar.SelectedIndex].Page);
-    }
-
-    private void MinimizeButton_Click(object? sender, RoutedEventArgs e)
-    {
-        WindowState = WindowState.Minimized;
-    }
-
-    private void MaximizeButton_Click(object? sender, RoutedEventArgs e)
-    {
-        if (WindowState == WindowState.Maximized)
-        {
-            WindowState = WindowState.Normal;
-            MaximizeIcon.Text = "\uE922";
-        }
-        else
-        {
-            WindowState = WindowState.Maximized;
-            MaximizeIcon.Text = "\uE923";
-        }
-    }
-
-    private void CloseButton_Click(object? sender, RoutedEventArgs e)
-    {
-        Close();
-    }
-    
-    // FIXME
-    private void CloseButton_OnPointerEntered(object? sender, PointerEventArgs e)
-    {
-        CloseButton.Background = new SolidColorBrush(Color.FromRgb(232, 17, 35));
-        CloseButton.BorderBrush = new SolidColorBrush(Color.FromRgb(232, 17, 35));
-    }
-    
-    // FIXME
-    private void CloseButton_OnPointerExited(object? sender, PointerEventArgs e)
-    {
-        CloseButton.Background = Brushes.Transparent;
-        CloseButton.BorderBrush = Brushes.Transparent;
-    }
-    
-    // Window Drag
-    
-    private bool _isPointerPressed;
-    private PixelPoint _startPosition = new(0, 0);
-    private Point _mouseOffsetToOrigin = new(0, 0);
-
-    private void TitleGrid_StartListenForDrag(object? sender, PointerPressedEventArgs e)
-    {
-        _startPosition = Position;
-        _mouseOffsetToOrigin = e.GetPosition(this);
-        _isPointerPressed = true;
-    }
-
-    private void TitleGrid_HandlePotentialDrag(object? sender, PointerEventArgs e)
-    {
-        if (!_isPointerPressed)
-        {
-            return;
-        }
-        
-        var pos = e.GetPosition(this);
-        _startPosition = new PixelPoint(
-            (int) (_startPosition.X + pos.X - _mouseOffsetToOrigin.X), 
-            (int) (_startPosition.Y + pos.Y - _mouseOffsetToOrigin.Y));
-        Position = _startPosition;
-    }
-
-    private void TitleGrid_HandlePotentialDrop(object? sender, PointerReleasedEventArgs e)
-    {
-        var pos = e.GetPosition(this);
-        _startPosition = new PixelPoint(
-            (int) (_startPosition.X + pos.X - _mouseOffsetToOrigin.X), 
-            (int) (_startPosition.Y + pos.Y - _mouseOffsetToOrigin.Y));
-        Position = _startPosition;
-        _isPointerPressed = false;
     }
 }
